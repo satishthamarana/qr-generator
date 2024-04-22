93cc3413ed73cb803a69b623451b157c03666100
satishthamarana <satish.thamarana@gmail.com>
2024-04-23 00:22:14 +0530

    The updated and successfully tested code

--- qrGenerator.py
+++ qrGenerator.py
@@ -6,10 +6,14 @@ from tkinter import *
 # This Function is responsible to take the input -> Convert it to Image Code -> Convert Image code to png.
 def get_code():
     data_var = data.get()
-    qr = pyqrcode.create(str(data_var))
-    qr.png('code.png', scale=6)
+    try:
+        qr = pyqrcode.create(str(data_var))
+        qr.png('code.png', scale=10)
+        print("QR Code generated successfully!")
+    except Exception as e:
+        print("Error generating QR Code:", e)
 
-#Get a Tk window of 400 * 200
+# Get a Tk window of 400 * 200
 base = Tk()
 base.geometry("400x200")
 base.title("QR Code Generator")
@@ -19,10 +23,10 @@ data = StringVar()
 
 # Field to input text
 dataEntry = Entry(textvariable=data, width="30")
-dataEntry.place(x=80,y=50)
+dataEntry.place(x=80, y=50)
 
 # Call get_code() on click
-button = Button(base,text="Get Code",command=get_code,width="30",height="2",bg="grey")
-button.place(x=80,y=100)
+button = Button(base, text="Get Code", command=get_code, width="30", height="2", bg="grey")
+button.place(x=80, y=100)
 
 base.mainloop()
